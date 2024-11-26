e2dd4e41a94acb0eb604ba1027dfe5d1dac5987f
Vice <73925257+you3957987@users.noreply.github.com>
2024-11-26 11:23:04 +0900

    ㅁㅁㅁ

---
+++ Assets/Script/ScenLoadDirector.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ScenLoadDirector : MonoBehaviour
+{
+    public void LoadScene_TO_MainGame()
+    {
+        SceneManager.LoadScene("MainGame");
+    }
+
+    public void LoadScene_TO_Start()
+    {
+        SceneManager.LoadScene("start");
+    }
+}
---
+++ Assets/Script/TimerDirector.cs
@@ -0,0 +1,52 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerDirector : MonoBehaviour
+{
+
+    public TMP_Text timer_ui;
+    float timer = 10.00f;
+
+    public bool game_start = false;
+    public GameDirector gameDirector;
+    public GameObject game_end_ui;
+    public bool game_end = false;
+
+
+    void Start()
+    {
+        UpdateTimerUI();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.L))  // G키를 누르면 타이머 작동
+        {
+            game_start = true;
+        }
+
+        if (timer > 0 && game_start)
+        {
+            timer -= Time.deltaTime; // 타이머 감소
+            if (timer < 0) timer = 0; // 타이머가 0 이하로 내려가지 않도록 제한
+            UpdateTimerUI();
+        }
+        
+        if( timer == 0 || gameDirector.current_max_line_weight <= 0)
+        {
+            game_end_ui.gameObject.SetActive(true);
+            Debug.Log("Game End");
+            game_end = true;
+        }
+
+    }
+    void UpdateTimerUI()
+    {
+        // 소수점 2자리까지 표시
+        timer_ui.text = timer.ToString("F2");
+    }
+
+
+}
